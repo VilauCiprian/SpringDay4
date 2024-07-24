6b275402074398525c4927a4207b6513d7adf4f2
unknown <vilauweb@vilauweb.ro>
2024-07-24 20:39:40 +0300

    Ex 1 from parameters.

--- SpringDay4/src/main/java/pl/coderslab/RandomController.java
+++ SpringDay4/src/main/java/pl/coderslab/RandomController.java
@@ -2,6 +2,7 @@ package pl.coderslab;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Random;
@@ -10,9 +11,17 @@ import java.util.Random;
 public class RandomController {
     @GetMapping("/showRandom")
     @ResponseBody
-    public String showRandom(){
+    public String showRandom() {
         Random random = new Random();
         int randomInt = random.nextInt(100);
-        return "The number drawn: "+randomInt;
+        return "The number drawn: " + randomInt;
+    }
+
+    @GetMapping("/random/{max}")
+    @ResponseBody
+    public String ex1Parameters(@PathVariable int max) {
+        Random random = new Random();
+        int randomInt = random.nextInt(max);
+        return "User entered the value " + max + ". The following number was drawn: . " + randomInt;
     }
 }
