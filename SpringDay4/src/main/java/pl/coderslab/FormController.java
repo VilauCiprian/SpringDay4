38b2dc3c7bd26b5507b1abd5ed25d827c63a18ff
unknown <vilauweb@vilauweb.ro>
2024-07-26 20:46:00 +0300

    Slide-uri cookies and sessions.

--- SpringDay4/src/main/java/pl/coderslab/FormController.java
+++ SpringDay4/src/main/java/pl/coderslab/FormController.java
@@ -1,10 +1,7 @@
 package pl.coderslab;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -18,7 +15,14 @@ public class FormController {
       return "form";
     }
     @PostMapping("/form")
-    public String postForm(@RequestParam("paramName") String name, @RequestParam String paramDate){
+    public String postForm(@RequestParam("paramName") String name, @RequestParam String paramDate,
+                           @CookieValue("cookie1") String cookie1){
+
+        // Am setat setPath pentru cookie2 /helloView dar aici sunt in alt path: /first/form.
+        // cookie1 este disponibil fiindca am setat setPath("/") adica root.
+
+        System.out.println(cookie1);
+
         System.out.println(name);
         DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         LocalDate date =LocalDate.parse(paramDate, format);
--- SpringDay4/src/main/java/pl/coderslab/HelloController.java
+++ SpringDay4/src/main/java/pl/coderslab/HelloController.java
@@ -2,14 +2,22 @@ package pl.coderslab;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class HelloController {
 
     @GetMapping("/helloView/{color}/{backgroundColor}")
-    public String helloView(@PathVariable String color, @PathVariable String backgroundColor, Model model) {
+    public String helloView(@PathVariable String color, @PathVariable String backgroundColor,
+                            Model model,
+                            @CookieValue("cookie1") String cookie1,
+                            @CookieValue("cookie2") String cookie2) {
+
+        System.out.println(cookie1);
+        System.out.println(cookie2);
 
         model.addAttribute("color", color);
         model.addAttribute("backgroundColor", backgroundColor);
@@ -17,6 +25,18 @@ public class HelloController {
         // business logic pentru color si backgroundColor:
 
         return "home";
+    }
+
+    @RequestMapping("/cookieSet")
+    @ResponseBody
+    public String homeJsp(HttpServletResponse response){
+        Cookie cookie1 = new Cookie("cookie1", "value1");
+        Cookie cookie2 = new Cookie("cookie2", "value2");
+        cookie1.setPath("/");
+        cookie2.setPath("/helloView");
+        response.addCookie(cookie1);
+        response.addCookie(cookie2);
+        return "setCookies";
     }
 
 }
